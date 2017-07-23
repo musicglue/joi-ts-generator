f269ead5d70eeb3b112436a5c4b29b92476b72cf
Lee Henson <lee.m.henson@gmail.com>
2017-07-23 20:40:38 +0100

    fix: uniq alternatives too to remove duplicates in outputted types

--- src/schemaVisitor/visitAlternatives.ts
+++ src/schemaVisitor/visitAlternatives.ts
@@ -1,4 +1,5 @@
 import { none, some } from "fp-ts/lib/Option";
+import { uniq } from "lodash";
 import { nameFromNotes } from "./naming";
 import { UnionType, VisitedType, Visitor } from "./types";
 
@@ -7,16 +8,18 @@ export const visitAlternatives: Visitor = visitSchema => schema => {
     return none;
   }
 
+  const matches = (schema._inner as any).matches as any[];
+
   // tslint:disable-next-line:no-shadowed-variable
-  const validAlternatives = (schema._inner as any).matches.map(({ schema }: { schema: any }) =>
-    nameFromNotes(schema) || schema._type);
+  const validAlternatives = uniq(matches.map(({ schema }: { schema: any }) =>
+    nameFromNotes(schema) || (schema._type as string)));
 
   if (validAlternatives.length === 0) {
     return none;
   }
 
   const typesUnion: UnionType = {
-    alternatives: validAlternatives,
+    alternatives: validAlternatives.sort(),
     kind: "union",
   };
 
