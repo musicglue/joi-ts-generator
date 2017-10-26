1c8e317190b38095952247e948cccf35a6bf5be9
Jonas Berlin <jonas.berlin@nitorcreations.com>
2017-10-26 12:42:32 +0200

    Use newer unquoted-property-validator which includes typescript typings

--- src/@types/unquoted-property-validator.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "unquoted-property-validator" {
-    function unquotedValidator(propertyName: string): {
-        needsQuotes: boolean,
-        needsBrackets: boolean,
-        es3Warning: boolean,
-        quotedValue: string,
-    };
-    export = unquotedValidator;
-}
